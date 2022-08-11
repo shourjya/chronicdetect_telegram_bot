e3a014a9a18d2adde9c9c77cec75badf04ae0adc
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-11 01:06:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from flask import Response
 import requests
  
 app = Flask(__name__)
+TELEGRAM_API_TOKEN = "5531087741:AAHNuNbYL267FWRBGHrKkblzPVo5PREIPC4"
  
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        msg = request.get_json()
-        print(msg)
-       
-        return Response('ok', status=200)
-    else:
-        return "<h1>Welcome to hell!</h1>"
- 
- 
-if __name__ == '__main__':
-   app.run(debug=True)
+ def send_telegram_message(chat_id, text):
+ 	request.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}}/sendMessage", json={
+ 		"chat_id": chat_id,
+ 		"text":text
+ 	})
+
+@app.route(f"/{TELEGRAM_API_TOKEN}", methods=["POST"])
+def bot_webhook():
+    payload = request.json
+    message = payload["message"]
+    chat_id = message["chat"]["id"]
+    text = message.get("text","")
+    print("Message received",text)
+    answer = "Hello!"
+    send_telegram_message(chat_id,answer)
+    return {}, 200
