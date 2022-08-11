2f3721a51501728a992717d66afbcc6e497d461d
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-11 01:11:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 TELEGRAM_API_TOKEN = "5531087741:AAHNuNbYL267FWRBGHrKkblzPVo5PREIPC4"
  
  def send_telegram_message(chat_id, text):
- 	request.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}}/sendMessage", json={
+ 	request.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage", json={
  		"chat_id": chat_id,
  		"text":text
+
  	})
 
 @app.route(f"/{TELEGRAM_API_TOKEN}", methods=["POST"])
@@ -21,4 +22,7 @@ def bot_webhook():
     print("Message received",text)
     answer = "Hello!"
     send_telegram_message(chat_id,answer)
-    return {}, 200
+    return {}, 200 
+ 
+if __name__ == '__main__':
+   app.run(port=5000,debug=True)
