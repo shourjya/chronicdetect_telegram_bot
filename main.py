9b40c011fac69af1c12a51a0806d743792296623
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-17 22:21:54 +0100

    add a y n question

--- main.py
+++ main.py
@@ -26,6 +26,16 @@ def bot_webhook():
     send_telegram_message(chat_id,answer)
     answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
     send_telegram_message(chat_id,answer)
+
+    answer = "Do you want to continue? [Y/N]"
+    send_telegram_message(chat_id,answer)
+
+    payload1 = request.json
+    message1 = payload1["message"]
+    letsgo = message1.get("text","")
+    print("User Status",letsgo)
+
+
     return {}, 200
   	
 if __name__ == '__main__':
