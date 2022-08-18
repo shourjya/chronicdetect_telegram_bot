a5f6557dd630766033e841cb169cbfbc8712ea8c
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-18 11:29:06 +0100

    added if elif and else loop

--- main.py
+++ main.py
@@ -15,26 +15,44 @@ def send_telegram_message(chat_id, text):
 @app.route(f"/{TELEGRAM_API_TOKEN}", methods=["GET","POST"])
 def bot_webhook():
     try :
-        payload = request.json
-        message = payload["message"]
-        chat_id = message["chat"]["id"]
-        text = message.get("text","")
-        print("Message received",text)
 
-        answer = "Welcome to Chronic Detect DTx Bot!"
-        send_telegram_message(chat_id,answer)
-        answer = "The bot will ask you a set of questions about your medical history and symptoms."
-        send_telegram_message(chat_id,answer)
-        answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
-        send_telegram_message(chat_id,answer)
+        if status == 1
+            print("status 0")
 
-        answer = "Do you want to continue? [Y/N]"
-        send_telegram_message(chat_id,answer)
+            answer = "Do you want to continue? [Y/N]"
+            send_telegram_message(chat_id,answer)
 
-        payload1 = request.json
-        message1 = payload1["message"]
-        letsgo = message1.get("text","")
-        print("User Status",letsgo)
+            payload1 = request.json
+            message1 = payload1["message"]
+            letsgo = message1.get("text","")
+            print("User Status",letsgo)
+
+            status = 2
+
+        elif status == 2
+            print("satus 2")
+
+        elif status == 3
+            print("satus 3")
+
+        elif status == 4
+            print("satus 4")
+        else
+            payload = request.json
+            message = payload["message"]
+            chat_id = message["chat"]["id"]
+            text = message.get("text","")
+            print("Message received",text)
+
+            answer = "Welcome to Chronic Detect DTx Bot!"
+            send_telegram_message(chat_id,answer)
+            answer = "The bot will ask you a set of questions about your medical history and symptoms."
+            send_telegram_message(chat_id,answer)
+            answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
+            send_telegram_message(chat_id,answer)
+
+            status = 1
+
 
     except Exception as e:
         print(e)
