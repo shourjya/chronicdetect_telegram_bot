4c5188168e262251b4fabfc915a88fa4edef26ec
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-18 11:22:15 +0100

    added try catch

--- main.py
+++ main.py
@@ -14,27 +14,30 @@ def send_telegram_message(chat_id, text):
 
 @app.route(f"/{TELEGRAM_API_TOKEN}", methods=["GET","POST"])
 def bot_webhook():
-    payload = request.json
-    message = payload["message"]
-    chat_id = message["chat"]["id"]
-    text = message.get("text","")
-    print("Message received",text)
+    try :
+        payload = request.json
+        message = payload["message"]
+        chat_id = message["chat"]["id"]
+        text = message.get("text","")
+        print("Message received",text)
 
-    answer = "Welcome to Chronic Detect DTx Bot!"
-    send_telegram_message(chat_id,answer)
-    answer = "The bot will ask you a set of questions about your medical history and symptoms."
-    send_telegram_message(chat_id,answer)
-    answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
-    send_telegram_message(chat_id,answer)
+        answer = "Welcome to Chronic Detect DTx Bot!"
+        send_telegram_message(chat_id,answer)
+        answer = "The bot will ask you a set of questions about your medical history and symptoms."
+        send_telegram_message(chat_id,answer)
+        answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
+        send_telegram_message(chat_id,answer)
 
-    answer = "Do you want to continue? [Y/N]"
-    send_telegram_message(chat_id,answer)
+        answer = "Do you want to continue? [Y/N]"
+        send_telegram_message(chat_id,answer)
 
-    payload1 = request.json
-    message1 = payload1["message"]
-    letsgo = message1.get("text","")
-    print("User Status",letsgo)
+        payload1 = request.json
+        message1 = payload1["message"]
+        letsgo = message1.get("text","")
+        print("User Status",letsgo)
 
+    except Exception as e:
+        print(e)
 
     return {}, 200
   	
