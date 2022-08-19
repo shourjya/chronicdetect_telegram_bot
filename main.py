8aa2596cc11e44589081fbce2b6df74690ec8ce5
shourjya sanyal <shourjya@users.noreply.github.com>
2022-08-19 19:37:04 +0100

    Added statys 1 to 7

--- main.py
+++ main.py
@@ -22,15 +22,16 @@ def bot_webhook():
         text = message.get("text","")
         print("Message received",text)
 
-        answer = "Welcome to Chronic Detect DTx Bot!"
-        send_telegram_message(chat_id,answer)
-        answer = "The bot will ask you a set of questions about your medical history and symptoms."
-        send_telegram_message(chat_id,answer)
-        answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
-        send_telegram_message(chat_id,answer)
+        # if (text != "Y","N") | (status == 8):
+        #     answer = "Welcome to Chronic Detect DTx Bot!"
+        #     send_telegram_message(chat_id,answer)
+        #     answer = "The bot will ask you a set of questions about your medical history and symptoms."
+        #     send_telegram_message(chat_id,answer)
+        #     answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
+        #     send_telegram_message(chat_id,answer)
 
-        answer = "Do you want to continue? [Y/N]"
-        send_telegram_message(chat_id,answer)
+        #     answer = "Do you want to continue? [Y/N]"
+        #     send_telegram_message(chat_id,answer)
 
         if text == "Y":
             print("status 1")
@@ -42,14 +43,61 @@ def bot_webhook():
 
             status = 2
 
+        if text == "N":
+            print("status 10")
+
+            answer = "Sorry to hear that!"
+            send_telegram_message(chat_id,answer)
+
+            status = 8
+
         if status == 2:
-            print("satus 2")
+            print("status 2")
 
             age = text
             send_telegram_message(chat_id,"What is your sex? [M/F]")
 
             status = 3
 
+        if status == 3:
+            print("status 2")
+
+            sex = text
+            send_telegram_message(chat_id,"WAre you a smoker? [Smoker/Non-Smoker]")
+
+            status = 4
+
+        if status == 4:
+            print("status 2")
+
+            age = text
+            send_telegram_message(chat_id,"Do you have diabetes? [No/Type 1/Type 2]")
+
+            status = 5
+
+        if status == 5:
+            print("status 2")
+
+            age = text
+            send_telegram_message(chat_id,"Do you have high blood cholesterol? [Yes/No]")
+
+            status = 6
+
+        if status == 6:
+            print("status 2")
+
+            age = text
+            send_telegram_message(chat_id,"Do you have high blood pressure? [Yes/No]")
+
+            status = 7
+
+        if status == 7:
+            print("status 2")
+
+            age = text
+            send_telegram_message(chat_id,"Do you have take blood pressure medication? [Yes/No]")
+
+            status = 8
     except Exception as e:
         print(e)
 
