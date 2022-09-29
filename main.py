3ec5f702926cfcb7be891aaf3ce78ae221655ca3
shourjya sanyal <shourjya@users.noreply.github.com>
2022-09-29 16:37:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import request
 from flask import Response
 import requests
 import json
-
+ 
 app = Flask(__name__)
 TELEGRAM_API_TOKEN = "5531087741:AAHNuNbYL267FWRBGHrKkblzPVo5PREIPC4"
  
@@ -13,11 +13,46 @@ def send_telegram_message(chat_id, text):
         "text":text
  	})
 
-def continue_question(chat_id):
+def ask_age(chat_id):
     requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
         "chat_id": chat_id,
-        "question": "Do you want to continue",
-        "options": json.dumps(["Y","N"])
+        "question": "What is your age",
+        "options": json.dumps(["Above 40-65","Below 65-79"])
+    })
+
+def ask_gender(chat_id):
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
+        "chat_id": chat_id,
+        "question": "What is your gender",
+        "options": json.dumps(["Male","Female"])
+    })
+
+def ask_smoker(chat_id):
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
+        "chat_id": chat_id,
+        "question": "Are you a smoker",
+        "options": json.dumps(["Current","Former","Never"])
+    })
+
+def ask_diabetes(chat_id):
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
+        "chat_id": chat_id,
+        "question": "History of Diabetes? ",
+        "options": json.dumps(["No","Type 1","Type 2"])
+    })
+
+def ask_hypertension(chat_id):
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
+        "chat_id": chat_id,
+        "question": "History of Hyperternsion? ",
+        "options": json.dumps(["Yes","No"])
+    })
+
+def ask_hypertension_dx(chat_id):
+    requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendPoll", json={
+        "chat_id": chat_id,
+        "question": "Do you take medication for Hyperternsion? ",
+        "options": json.dumps(["Yes","No"])
     })
 
 @app.route(f"/{TELEGRAM_API_TOKEN}", methods=['POST','GET'])
@@ -38,9 +73,16 @@ def bot_webhook():
             send_telegram_message(chat_id,answer)
             answer = "The bot will then give you a risk score for developing heart attack or stroke within the next 10 years."
             send_telegram_message(chat_id,answer)
+            answer = "Do you want to start [Y/N]"
+            send_telegram_message(chat_id,answer)
 
-            continue_question(chat_id)
-
+        if (text == 'Y'):
+            ask_age(chat_id)
+            ask_gender(chat_id)
+            ask_smoker(chat_id)
+            ask_diabetes(chat_id)
+            ask_hypertension(chat_id)
+            ask_hypertension_dx(chat_id)
 
     except Exception as e:
         print(e)
